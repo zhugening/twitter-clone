0d57eac46fbd5ac799e42568c581a70c6720c57d
zhuge_ning17@hotmail.com <zhuge_ning17@hotmail.com>
2022-09-07 22:01:15 +0700

    sent data to firebase & loading

--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,53 @@
 import { PhotographIcon, EmojiHappyIcon } from "@heroicons/react/outline"
 import React from 'react'
 import { useSession , signOut } from "next-auth/react"
+import { useState, useRef } from "react"
+import { db } from "../firebase";
+import { addDoc , collection , serverTimestamp } from "firebase/firestore";
+import { XIcon } from "@heroicons/react/solid";
 
 export default function Input() {
   const {data: session} = useSession()
-  
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const filePickerRef = useRef(null);
+
+  const sendPost = async ()=>{
+    if(loading) return;
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      id: session.user.uid,
+      text: input,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+      name: session.user.name,
+      username: session.user.username
+    });
+    const imageRef = ref(storage, `posts/${docRef.id}/image`)
+    if(selectedFile){
+      await uploadString(imageRef, selectedFile, "data_url").then(async()=>{
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id),{
+          image: downloadURL,
+        })
+      })
+    }
+    setInput("");
+    setSelectedFile(null);
+    setLoading(false);
+  }
+
+  const addImageToPost = (e) =>{
+    const reader = new FileReader();
+    if(e.target.files[0]){
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent)=>{
+      setSelectedFile(readerEvent.target.result);
+    }
+  }
 
   return (
     <>
@@ -17,14 +60,43 @@ export default function Input() {
         className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"/>
         <div className='w-full'>
             <div className='w-full divide-y divide-gray-200'>
-            <textarea className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?"></textarea>
+            <textarea 
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" 
+            rows="2" 
+            placeholder="What's happening?"
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
+            ></textarea>
             </div>
+            {selectedFile && (
+              <div className="relative">
+                <XIcon 
+                onClick={()=>setSelectedFile(null)} 
+                className="h-7 text-black absolute cursor-pointer shadow-md shadow-white rounded-full"/>
+                <img src={selectedFile} className={`${loading && "animate-pulse"}`}/>
+              </div>
+            )}
             <div className='flex items-center justify-between pt-2.5'>
-                <div className='flex'>
+              {!loading &&(
+                <>
+                  <div className='flex'>
+                  <div className="" onClick={() =>filePickerRef.current.click()}>
                     <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
+                    <input 
+                    type="file" 
+                    hidden ref={filePickerRef} 
+                    onChange={addImageToPost}/>
+                    </div>
                     <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
                 </div>
-                <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50" disabled>Tweet</button>
+                <button
+                onClick={sendPost}
+                disabled={!input.trim()} 
+                className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50" 
+                disabled>Tweet</button>
+                </>
+              )}
+                
             </div>
         </div>
     </div>
