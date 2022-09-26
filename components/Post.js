4a62a9764fb1e65b5f5dc00e037479fe6ae23ff2
zhuge_ning17@hotmail.com <zhuge_ning17@hotmail.com>
2022-09-26 15:01:16 +0700

    complete the comment component

--- components/Post.js
+++ components/Post.js
@@ -94,14 +94,18 @@ export default function Post({ post, id }) {
         </div>
 
         {/* post text */}
-        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2]">
+        <p 
+        onClick={()=>router.push(`/posts/${id}`)}
+        className="text-gray-800 text-[15px sm:text-[16px] mb-2]">
           {post?.data()?.text}
         </p>
 
         {/* post image */}
-        <img className="rounded-2xl mr-2 " 
-          src ={post?.data()?.image} 
-          alt=""/>
+        <img 
+        onClick={()=>router.push(`/posts/${id}`)}
+        className="rounded-2xl mr-2 " 
+        src ={post?.data()?.image} 
+        alt=""/>
 
         {/* post icon */}
         <div className="flex justify-between text-gray-500 p-2">
